149cb105237a81c12dc8e45754990f56bd2c7bf2
Alejandro <alejandromaier@al.uap.edu.ar>
2015-09-27 13:27:39 -0300

    Fifth commit

--- ejercicios tp/Pelicula.hs
+++ ejercicios tp/Pelicula.hs
@@ -22,30 +22,26 @@ data Pelicula = Pelicula
 -- PARA MI ESTA MAL ACA LAS PELICULAS deberias ser como clientes, definirlas y luego usarlas, me parece.. <--VER
 
 --          |Tipo    |id |    titulo pelicula             | alquilada?| Videclub    |   categoria   |   cliente?  |
-pelicula1  = Pelicula 1   "At the Mountains of Madness"       True      V.videoclub1      "Ficcion"     
-pelicula2  = Pelicula 2   "The Case of Charles Dexter Ward"   True      V.videoclub1      "Ficcion"      
-pelicula3  = Pelicula 3   "Herbert West -- The Re-animator"   True      V.videoclub1      "Ficcion"      
-pelicula4  = Pelicula 4   "The Call"                          True      V.videoclub1      "Miedo"      
-pelicula5  = Pelicula 5   "Frozen"                            True      V.videoclub2      "Fantasia"      
-pelicula6  = Pelicula 6   "Fight Club"                        False     V.videoclub2      "Accion"      
+pelicula1  = Pelicula 1   "At the Mountains of Madness"       True      V.videoclub1      "Ficcion"
+pelicula2  = Pelicula 2   "The Case of Charles Dexter Ward"   True      V.videoclub1      "Ficcion"
+pelicula3  = Pelicula 3   "Herbert West -- The Re-animator"   True      V.videoclub1      "Ficcion"
+pelicula4  = Pelicula 4   "The Call"                          True      V.videoclub1      "Miedo"
+pelicula5  = Pelicula 5   "Frozen"                            True      V.videoclub2      "Fantasia"
+pelicula6  = Pelicula 6   "Fight Club"                        False     V.videoclub2      "Accion"
 pelicula7  = Pelicula 7   "At the Mountains of Madness"       False     V.videoclub1      "Ficcion"
 pelicula8  = Pelicula 8   "Frozen"                            False     V.videoclub1      "Fantasia"
 pelicula9  = Pelicula 9   "The Call"                          False     V.videoclub2      "Miedo"
 
-peliculas  = [pelicula9 , pelicula6, pelicula3, pelicula4, pelicula5, pelicula2, pelicula7, pelicula8,pelicula1]
+peliculas  = [pelicula1 , pelicula2, pelicula3, pelicula4, pelicula5, pelicula6, pelicula7, pelicula8,pelicula9]
 
--- peliculasNoAlquiladas :: Pelicula -> String
--- peliculasNoAlquiladas (Pelicula {_id=i,_titulo=t,_alquilada=a}) = case (a) of False -> "Pelicula: "++t 
-                                                                              -- True  ->  (peliculas !! (i+1)) peliculasNoAlquiladas (peliculas!! (i+1))
-peliculasNoAlquiladas :: Pelicula -> String
-peliculasNoAlquiladas (Pelicula {_alquilada=a,_titulo=t,_id=i}) = case a of False -> "Pelicula -> "++ t 
-                                                                            True  -> Nothing
+tituloPeliculas :: Pelicula -> String
+tituloPeliculas (Pelicula {_titulo = t}) = "Pelicula: "++t
 
 tellPelicula :: Pelicula -> String
 tellPelicula (Pelicula {_id = i, _titulo = t, _alquilada=a}) = "Pelicula "++ show i ++ ": " ++ t ++ " -> "++estadoPelicula a
 
 tituloPelicula :: Pelicula -> String
-tituloPelicula (Pelicula {_titulo = t}) = t 
+tituloPelicula (Pelicula {_titulo = t}) = t
 
 copiasPelicula :: Pelicula -> String
 copiasPelicula (Pelicula {_id = i,_titulo = t}) = "Pelicula: "++ show i++ ": "++ t++ " | Cantidad de copias: "++ show(cantidad_copias t)
@@ -54,25 +50,27 @@ videoclubPelicula :: Pelicula -> String
 videoclubPelicula (Pelicula {_titulo =t, _videoclub=v,_alquilada=a}) = "La pelicula: "
                                                                        ++t
                                                                        ++ "-> se encuentra en el videclub: "
-                                                                       ++ V._nombre v 
-                                                                       ++"-> El estado de la pelicula es: " 
+                                                                       ++ V._nombre v
+                                                                       ++"-> El estado de la pelicula es: "
                                                                        ++ estadoPelicula a
+-- categorias = 
 
-peliculas_count = length peliculas
+peliculas_na = filter (not . _alquilada) peliculas                                      --Devuelve una lista de peliculas que no estan alquiladas
+
+peliculas_count = length peliculas -1                                                   --Devuelve el numero de peliculas que hay, como valor utilizable
 
 estadoPelicula estado = if estado == False then " Disponible" else " No disponible"
 
-existe_pelicula titulo = find (==titulo) $map (tituloPelicula) peliculas
+existe_pelicula titulo = find (==titulo) $map (tituloPelicula) peliculas                --Devuelve un bool si existe la pelicula con ese titulo
 
-cantidad_copias titulo = length $filter (==titulo) $map (tituloPelicula) peliculas 
+cantidad_copias titulo = length $filter (==titulo) $map (tituloPelicula) peliculas      --Devuelve el numero de copias de una pelicula segun el titulo
 
 contar_pelicula titulo = case (existe_pelicula titulo) of Nothing     -> "No existe esa pelicula"
-                                                          Just titulo -> titulo ++" tiene " ++ if (cantidad_copias titulo) == 1 
-                                                            then show(cantidad_copias titulo)++" copia." 
-                                                            else show (cantidad_copias titulo)++" copias." 
+                                                          Just titulo -> titulo ++" tiene " ++ if (cantidad_copias titulo) == 1
+                                                            then show(cantidad_copias titulo)++" copia."
+                                                            else show (cantidad_copias titulo)++" copias."
 
 listar_copias_peliculas = mapM_  print $map (copiasPelicula)        peliculas
 listar_peliculas        = mapM_  print $map (tellPelicula)          peliculas
 listar_peliculas_vc     = mapM_  print $map (videoclubPelicula)     peliculas
-listar_peliculas_na     = mapM_  print $map (peliculasNoAlquiladas) peliculas
-
+listar_peliculas_na     = mapM_  print $map (tituloPeliculas)       peliculas_na
--- ejercicios tp/main.hs
+++ ejercicios tp/main.hs
@@ -5,8 +5,8 @@ import qualified VideoClub   as V
 import qualified Cliente     as C
 import qualified Pelicula    as P
 import qualified Alquiler    as A
- 
--- Listas 
+
+-- Listas
 
 listar_clientes         = C.listar_clientes
 listar_peliculas        = P.listar_peliculas
@@ -17,9 +17,9 @@ listar_peliculas_na     = P.listar_peliculas_na
 --funciones
 -- elem :: (Eq a) => a -> [ a ] -> Bool
 -- elem unElemento unaLista = any (unElemento==) unaLista
-                                                                                                                    
+
 -- buscar :: (Eq a) => a -> [a] -> a
--- buscar nombre lista = map (==_nombre) clientes 
+-- buscar nombre lista = map (==_nombre) clientes
 
 -- buscar_en lista nombre = filter (==nombre) clientes
 
@@ -31,28 +31,28 @@ listar_peliculas_na     = P.listar_peliculas_na
 -- -- Usados como variables
 -- cantidad_clientes = length clientes
 -- --cantidad_copias_pelicula x = _copias (peliculas !! x)
--- nombre_pelicula x = _titulo (peliculas!! x) 
+-- nombre_pelicula x = _titulo (peliculas!! x)
 
 -- -- Todas lasP.peliculas de un cliente
 -- peliculas_cliente x = _alquileres (clientes!! x)
 
 
--- Se cuenta con los tipos compuestos: Videoclub, Cliente y Película, y las siguientes funciones: 
--- •   y  Clientes, que devuelve una lista con los clientes registrados en el videoclub. 
--- •   y  Películas, que devuelve una lista con las películas que están en el catálogo del videoclub, independientemente de si esta disponible o no. 
--- •   y  CantidadDeCopiasPelicula, que devuelve cuantas copias de una película tiene el videoclub (esta función se indefine, o sea da error, si la película no pertenece al catalogo del videoclub). 
--- •   n  CantidadDeCopias, que devuelve una lista con las películas y cuantas copias  tiene el videoclub. 
--- •     PelículasNoAlquiladas, que devuelve una lista de películas que no han sido alquiladas.
+-- Se cuenta con los tipos compuestos: Videoclub, Cliente y Película, y las siguientes funciones:
+-- •   y  Clientes, que devuelve una lista con los clientes registrados en el videoclub.
+-- •   y  Películas, que devuelve una lista con las películas que están en el catálogo del videoclub, independientemente de si esta disponible o no.
+-- •   y  CantidadDeCopiasPelicula, que devuelve cuantas copias de una película tiene el videoclub (esta función se indefine, o sea da error, si la película no pertenece al catalogo del videoclub).
+-- •   n  CantidadDeCopias, que devuelve una lista con las películas y cuantas copias  tiene el videoclub.
+-- •   y  PelículasNoAlquiladas, que devuelve una lista de películas que no han sido alquiladas.
 -- •     peliculasPorCategoria, que devuelve un listado de películas por categoría seleccionada.*
--- •     PelículasAlquiladasPorCliente, que devuelve una lista de películas que el cliente tiene alquiladas en el videoclub (esta función se indefine si el cliente no esta registrado en el videoclub). El cliente puede alquilar más de una copia de la misma película. 
--- •     Alquiló, que indica si un cliente de un videoclub alquiló una película en particular. 
--- •     CopiasAlquiladas, que devuelve la cantidad de copias de la película dada que están alquiladas en un videoclub. 
--- •     DisponibleParaAlquilar, que indica si una película esta disponible para ser alquilada en el videoclub (por ejemplo, una película puede estar en un videoclub pero no estar disponible porque todas sus copias están alquiladas, o no estar disponible porque no esta en el catálogo). 
+-- •     PelículasAlquiladasPorCliente, que devuelve una lista de películas que el cliente tiene alquiladas en el videoclub (esta función se indefine si el cliente no esta registrado en el videoclub). El cliente puede alquilar más de una copia de la misma película.
+-- •     Alquiló, que indica si un cliente de un videoclub alquiló una película en particular.
+-- •     CopiasAlquiladas, que devuelve la cantidad de copias de la película dada que están alquiladas en un videoclub.
+-- •     DisponibleParaAlquilar, que indica si una película esta disponible para ser alquilada en el videoclub (por ejemplo, una película puede estar en un videoclub pero no estar disponible porque todas sus copias están alquiladas, o no estar disponible porque no esta en el catálogo).
 -- •     CategoriasDisponibles, que devuelve una lista de categorías disponibles en el videoclub.*
 
 -- Especificar y definir las siguientes funciones:
 -- 1.    disponibleEnLaRed, que dice si la película está disponible en al menos alguno de los videoclubs de la red.
--- 2.    masAlquiladaEnLaRed, que devuelve la película más alquilada en todos los videoclubs de la red. 
+-- 2.    masAlquiladaEnLaRed, que devuelve la película más alquilada en todos los videoclubs de la red.
 -- 3.    ClientesRaros, que devuelve la lista de clientes que tienen alquilada la misma película en distintos videoclubs.
 -- 4.    CantidadTitulosOriginales, que devuelve una lista con las películas de todos los videoclubs, sin repetidos.
 -- 5.    peliculasDisponibles, que devuelve una lista con las películas disponibles, de entre los videoclubs de la red, para alquilar y el número de copias.
